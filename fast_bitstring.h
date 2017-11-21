649a590561b802f87df8108a858618232a699905
kenosis6971 <kenosis@gmail.com>
2017-11-21 09:12:57 -0800

    Reformat not using company tabbing.

--- fast_bitstring.h
+++ fast_bitstring.h
@@ -23,98 +23,98 @@
 class fast_bitstring {
 
 public:
-        typedef unsigned char byte;
+	typedef unsigned char byte;
 
-        // Construct bit array of all zero bits.
-        fast_bitstring(const size_t length_in_bytes) : BITS_PER_BYTE(8) {
-                blength = length_in_bytes * BITS_PER_BYTE;
-                barray = (byte *)calloc(blength, 1);
-        }
+	// Construct bit array of all zero bits.
+	fast_bitstring(const size_t length_in_bytes) : BITS_PER_BYTE(8) {
+		blength = length_in_bytes * BITS_PER_BYTE;
+		barray = (byte *)calloc(blength, 1);
+	}
 
-        // Construct bit array from given bit string backed in byte_array.
-        fast_bitstring(const byte byte_array[], const size_t length_in_bytes) : BITS_PER_BYTE(8) {
-                explode(byte_array, length_in_bytes);
-        }
-
-	// Length of bit string in bits.
-        inline size_t length() const { return blength; }
+	// Construct bit array from given bit string backed in byte_array.
+	fast_bitstring(const byte byte_array[], const size_t length_in_bytes) : BITS_PER_BYTE(8) {
+	explode(byte_array, length_in_bytes);
+	}
 
 	// Length of bit string in bits.
-        inline void clear() { memset((void *)barray, 0, blength); }
+	inline size_t length() const { return blength; }
+
+	// Length of bit string in bits.
+	inline void clear() { memset((void *)barray, 0, blength); }
 
 	// Array opperator to access bit[i].
-        inline byte &operator [](const size_t i) const {
-                return barray[i];
-        }
+	inline byte &operator [](const size_t i) const {
+		return barray[i];
+	}
 
 	// Convert internal byte per bit representation back to bits packed into
 	// given byte array.
-        size_t to_bitstring(byte *bytes, size_t offset=0, size_t num_bits=0) {
+	size_t to_bitstring(byte *bytes, size_t offset=0, size_t num_bits=0) {
 
-                if (num_bits == 0 || num_bits > blength)
-                        num_bits = blength;
+		if (num_bits == 0 || num_bits > blength)
+			num_bits = blength;
 
-                const size_t end = offset + num_bits;
-                register byte b;
-                register byte *ba_ptr = barray;
-                register byte *bp = bytes;
+		const size_t end = offset + num_bits;
+		register byte b;
+		register byte *bap = barray;
+		register byte *bp = bytes;
 
-                for (size_t i = offset; i < end; i += BITS_PER_BYTE) {
-                        b  = (*ba_ptr++ & 1) << 7;
-                        b |= (*ba_ptr++ & 1) << 6;
-                        b |= (*ba_ptr++ & 1) << 5;
-                        b |= (*ba_ptr++ & 1) << 4;
-                        b |= (*ba_ptr++ & 1) << 3;
-                        b |= (*ba_ptr++ & 1) << 2;
-                        b |= (*ba_ptr++ & 1) << 1;
-                        b |= (*ba_ptr++ & 1);
-                        *bp++ = b;
-                }
+		for (size_t i = offset; i < end; i += BITS_PER_BYTE) {
+			b  = (*bap++ & 1) << 7;
+			b |= (*bap++ & 1) << 6;
+			b |= (*bap++ & 1) << 5;
+			b |= (*bap++ & 1) << 4;
+			b |= (*bap++ & 1) << 3;
+			b |= (*bap++ & 1) << 2;
+			b |= (*bap++ & 1) << 1;
+			b |= (*bap++ & 1);
+			*bp++ = b;
+		}
 
-                return num_bits;
-        }
+		return num_bits;
+	}
 
 #ifdef FBS_DEBUG
-        void dump(void) const {
-                printf("Dumping...\n");
-                for (int i = 0; i < blength; ++i)
-                        printf ("%u ", barray[i]);
-				printf("\n");
-				fflush(stdout);
-        }
+	void dump(void) const {
+		printf("Dumping...\n");
+		for (int i = 0; i < blength; ++i)
+			printf ("%u ", barray[i]);
+		printf("\n");
+		fflush(stdout);
+	}
 #endif
 
 protected:
 
-        /*
-         * Given a byte array containing a packed string of bits, explode the bits
-         * into an array of bytes, one bit per byte.  Yes, this is an 8x increase
-         * in memory utlization but that is the classic time/space trade off for you.
-         */
-        void explode(const byte byte_array[], const size_t length) {
+	/*
+	 * Given a byte array containing a packed string of bits, explode the bits
+	 * into an array of bytes, one bit per byte.  Yes, this is an 8x increase
+	 * in memory utlization but that is the classic time/space trade off for you.
+	 */
+	void explode(const byte byte_array[], const size_t length) {
 
-                register byte b, mask, *ba;
-                int i, blen;
+		register byte b, mask, *ba;
+		int i, blen;
 
-                blength = length * BITS_PER_BYTE;
-                barray = (byte *)calloc(blength, 1);
+		blength = length * BITS_PER_BYTE;
+		barray = (byte *)calloc(blength, 1);
 
-                for (i = 0, ba = barray; i < length; ++i) {
-                        b = byte_array[i];
-                        for (mask = 0x80; mask != 0x0; mask >>= 1) {
-                                if (b & mask) *ba = 0x1;
-								++ba;
-                        }
-                }
-        }
+		for (i = 0, ba = barray; i < length; ++i) {
+			b = byte_array[i];
+			for (mask = 0x80; mask != 0x0; mask >>= 1) {
+				if (b & mask) *ba = 0x1;
+				++ba;
+			}
+		}
+	}
 
 private:
 
-        fast_bitstring() : BITS_PER_BYTE(8) {}
+	fast_bitstring() : BITS_PER_BYTE(8) {}
 
-		const size_t BITS_PER_BYTE;
-        size_t blength;
-        byte *barray;   // Array of bits, one per bit.
+	const size_t BITS_PER_BYTE;
+	size_t blength;
+	byte *barray;   // Array of bits, one per bit.
 
 };
 

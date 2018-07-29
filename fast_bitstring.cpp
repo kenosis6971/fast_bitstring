26493af32e87a1f29a89e28e32afd6518c794336
kenosis6971 <kenosis@gmail.com>
2018-07-29 12:52:03 -0700

    Add more complete copyright and trade secret disclaimers.

--- fast_bitstring.cpp
+++ fast_bitstring.cpp
@@ -1,9 +1,14 @@
 //
+// fast_bitstring.cpp
+//
 // Copyright (C) 2017-2018 Ken Hilton, all rights reserved.
 //
-//	fast_bitstring.cpp
+// The contents of this source code is protected by trade secret law and may not be viewed,
+// studied, compiled or otherwise utilized in any manner with out executing a binding non-
+// disclosure agreement including written permission of permissabe from both Ken Hilton and
+// Day Goodwin.  Furthermore, this source code contains both intellectual property and trade
+// secrets that are the exclusive propery of Ken Hilton and Day Goodwin, respectively.
 //
-
 
 #include "fast_bitstring.h"
 
--- test.cpp
+++ test.cpp
@@ -1,6 +1,14 @@
-/*
- *	test.cpp
- */
+//
+// test.cpp
+//
+// Copyright (C) 2017-2018 Ken Hilton, all rights reserved.
+//
+// The contents of this source code is protected by trade secret law and may not be viewed,
+// studied, compiled or otherwise utilized in any manner with out executing a binding non-
+// disclosure agreement including written permission of permissabe from both Ken Hilton and
+// Day Goodwin.  Furthermore, this source code contains both intellectual property and trade
+// secrets that are the exclusive propery of Ken Hilton and Day Goodwin, respectively.
+//
 
 #include <assert.h>
 #include <stdio.h>

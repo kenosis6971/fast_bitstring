da28f280ab24edbec60526a7b6271949571244db
kenosis6971 <kenosis@gmail.com>
2020-08-01 13:57:13 -0700

    Refactor purpose to readme.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# fast_bitstring
+
+Fast bit string converts a bit string packed into an array of bytes
+into an array of bytes, one byte for each bit of the of the source
+array of bits.  This representation is designed for two purposes only:
+speed and convenience of use, i.e., KISS.
--- fast_bitstring.h
+++ fast_bitstring.h
@@ -1,10 +1,5 @@
 /*
- *	fast_bitstring.h
- *
- *	Fast bit string converts a bit string packed into an array of bytes
- *	into an array of bytes, one byte for each bit of the of the source
- *	array of bits.  This representation is designed for two purposes only:
- *	speed and convenience of use, i.e., KISS.
+ * fast_bitstring.h
  *
  * Copyright (C) 2017-2020 Ken Hilton, all rights reserved.
  *

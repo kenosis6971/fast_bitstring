151f6cce763be71916ea2e8c687602baac136c17
kenosis6971 <kenosis@gmail.com>
2020-08-01 13:53:45 -0700

    Update copyright/trade secret declarations.

--- fast_bitstring.h
+++ fast_bitstring.h
@@ -6,13 +6,13 @@
  *	array of bits.  This representation is designed for two purposes only:
  *	speed and convenience of use, i.e., KISS.
  *
- *	Copyright (C) 2017-2018 Ken Hilton, all rights reserved.
- *	
- *	The contents of this source code is protected by trade secret law and may not be viewed,
- *	studied, compiled or otherwise utilized in any manner with out executing a binding non-
- *	disclosure agreement including written permission of permissabe from both Ken Hilton and
- *	Day Goodwin.  Furthermore, this source code contains both intellectual property and trade
- *	secrets that are the exclusive propery of Ken Hilton and Day Goodwin, respectively.
+ * Copyright (C) 2017-2020 Ken Hilton, all rights reserved.
+ *
+ * The contents of this source code is protected by trade secret law and may not be viewed,
+ * studied, compiled or otherwise utilized in any manner with out executing a binding non-
+ * disclosure agreement including written permission of permissible use from Ken Hilton.
+ * Furthermore, this source code contains both intellectual property and trade
+ * secrets that are the exclusive propery of Ken Hilton.
  */
 
 #ifndef _FAST_BITSTRING_H
--- test.h
+++ test.h
@@ -1,14 +1,13 @@
 //
 // test.h
 //
-//
-// Copyright (C) 2017-2018 Ken Hilton, all rights reserved.
+// Copyright (C) 2017-2020 Ken Hilton, all rights reserved.
 //
 // The contents of this source code is protected by trade secret law and may not be viewed,
 // studied, compiled or otherwise utilized in any manner with out executing a binding non-
-// disclosure agreement including written permission of permissabe from both Ken Hilton and
-// Day Goodwin.  Furthermore, this source code contains both intellectual property and trade
-// secrets that are the exclusive propery of Ken Hilton and Day Goodwin, respectively.
+// disclosure agreement including written permission of permissible use from Ken Hilton.
+// Furthermore, this source code contains both intellectual property and trade
+// secrets that are the exclusive propery of Ken Hilton.
 //
 #ifndef	_TEST_H
 #define	_TEST_H
